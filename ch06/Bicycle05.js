a722f6f1d5ea15c3d258916f435b4fdb8cd84398
Eddy Hernandez <eddy.josafat@basekit.com>
2016-04-04 17:54:31 +0100

    Creating the common abstract bicycle class: 2nd iteration

---
+++ ch06/Bicycle05.js
@@ -0,0 +1,57 @@
+class Bicycle {
+    constructor(options = {}) {
+        ({
+            size: this.size,
+        } = options);
+    }
+}
+
+export class RoadBike extends Bicycle {
+    constructor(options = {}) {
+        super(options);
+        ({
+            tape_color: this.tape_color,
+        } = options);
+    }
+
+    spares() {
+        return {
+            chain: '10-speed',
+            tire_size: '23',
+            tape_color: this.tape_color
+        };
+    }
+}
+
+export class MountainBike extends Bicycle {
+    constructor(options = {}) {
+        super(options);
+        ({
+            front_shock: this.front_shock,
+            rear_shock: this.rear_shock
+        } = options);
+    }
+
+    spares() {
+        let result = super.spares();
+        result.rear_shock = this.rear_shock;
+
+        return result;
+    }
+}
+
+if (require.main == module) {
+    const road_bike = new RoadBike({
+        size: 'M',
+        tape_color: 'red'
+    });
+    console.log(road_bike.size);
+
+    const mountain_bike = new MountainBike({
+        size: 'S',
+        front_shock: 'Manitou',
+        rear_shock: 'Fox'
+    });
+
+    console.log(mountain_bike.size);
+}
