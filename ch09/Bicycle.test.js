326b15e1e5a8b68c468fec66782e1c05930977c6
Eddy Hernandez <eddy.josafat@basekit.com>
2016-07-29 15:25:26 +0100

    Testing superclass enforcement

--- ch09/Bicycle.test.js
+++ ch09/Bicycle.test.js
@@ -1,8 +1,18 @@
 import Bicycle from './sources/Bicycle';
 import bicycleInterfaceTest from './test_includes/bicycle_interface.test';
 
+const assert = require('chai').assert;
+
 describe('Bicycle', () => {
     bicycleInterfaceTest(new Bicycle({
         tire_size: 0,
     }));
+
+    it('enforces subclasses to implement default_tire_size', () => {
+        const bicycle = new Bicycle({
+            tire_size: 0,
+        });
+
+        assert.throws(bicycle.default_tire_size, 'Implement default_tire_size()');
+    });
 });
