f4e5400ed8a0ad95f557cda4b329081ca9fdee90
Eddy Hernandez <eddy.josafat@basekit.com>
2016-07-29 16:05:33 +0100

    Test specialisation of each class

--- ch09/Bicycle.test.js
+++ ch09/Bicycle.test.js
@@ -1,7 +1,20 @@
 import Bicycle from './sources/Bicycle';
 import bicycleInterfaceTest from './test_includes/bicycle_interface.test';
+import bicycleSubclassTest from './test_includes/bicycle_subclass.test';
 
 const assert = require('chai').assert;
+
+class StubbedBike extends Bicycle {
+    default_tire_size() {
+        return 0;
+    }
+
+    local_spares() {
+        return {
+            saddle: 'painful',
+        };
+    }
+}
 
 describe('Bicycle', () => {
     bicycleInterfaceTest(new Bicycle({
@@ -15,4 +28,17 @@ describe('Bicycle', () => {
 
         assert.throws(bicycle.default_tire_size, 'Implement default_tire_size()');
     });
+
+    it('includes local_spares in spares', () => {
+        const bike = new StubbedBike();
+        assert.deepEqual(bike.spares(), {
+            tire_size: 0,
+            chain: '10-speed',
+            saddle: 'painful',
+        });
+    });
+});
+
+describe('StubbedBike', () => {
+    bicycleSubclassTest(new StubbedBike());
 });
--- ch09/RoadBike.test.js
+++ ch09/RoadBike.test.js
@@ -2,7 +2,17 @@ import RoadBike from './sources/RoadBike';
 import bicycleInterfaceTest from './test_includes/bicycle_interface.test';
 import bicycleSubclassTest from './test_includes/bicycle_subclass.test';
 
+const assert = require('chai').assert;
+
 describe('RoadBike', () => {
     bicycleInterfaceTest(new RoadBike());
     bicycleSubclassTest(new RoadBike());
+
+    it('puts tape color in local spares', () => {
+        const bike = new RoadBike({
+            tape_color: 'red',
+        });
+
+        assert.strictEqual(bike.local_spares().tape_color, 'red');
+    });
 });
